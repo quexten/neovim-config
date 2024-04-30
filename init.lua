bb99138184ec826efd518bc048366f4b04a0279b
Bernd Schoolmann <mail@quexten.com>
2024-04-30 20:32:35 +0200

    Add harpoon

--- init.lua
+++ init.lua
@@ -294,7 +294,30 @@ require('lazy').setup({
       vim.keymap.set({ 'n', 'x', 'o' }, 'Q', '<Plug>(leap-backward)')
     end,
   },
+  {
+      "ThePrimeagen/harpoon",
+      branch = "harpoon2",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      config = function()
+        local harpoon = require("harpoon")
 
+        -- REQUIRED
+        harpoon:setup()
+        -- REQUIRED
+
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+        -- Toggle previous & next buffers stored within Harpoon list
+        vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+        vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+      end,
+  },
   -- NOTE: Plugins can specify dependencies.
   --
   -- The dependencies are proper plugin specifications as well - anything
